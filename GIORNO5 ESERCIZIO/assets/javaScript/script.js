b62e4b1800012934f37025f733d7fe4055be11d5
gabritonce <126726082+gabritonce@users.noreply.github.com>
2023-04-03 16:23:17 +0200

    Add files via upload

---
+++ GIORNO5 ESERCIZIO/assets/javaScript/script.js
@@ -0,0 +1,5 @@
+// SENZA VARIABILI //
+document.getElementById('javascript').innerHTML = 'Output scritto da Javascript'
+
+alert('BENVENUTO/BENVENUTA NELLA PAGINA 2');
+alert('CLICCA DI NUOVO PER CONTINUARE')
---
+++ GIORNO5 ESERCIZIO/assets/javaScript/script2.js
@@ -0,0 +1,12 @@
+// CON VARIABILI 
+
+var altro_testo = 'Pantera scritto da JavaScript'
+document.getElementById('altrotesto2').innerHTML = altro_testo
+
+
+var C_riceto = 'criceto'
+var Giraffa = 'giraffa'
+var leone = 'leone'
+console.log(C_riceto)
+console.log(Giraffa)
+console.log(leone)
