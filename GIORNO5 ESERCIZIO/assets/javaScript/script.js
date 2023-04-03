47b8ac044cd516f4b3227e225e6259edd7ec22f2
gabritonce <126726082+gabritonce@users.noreply.github.com>
2023-04-03 16:23:02 +0200

    Delete GIORNO5 ESERCIZIO directory

--- GIORNO5 ESERCIZIO/assets/javaScript/script.js
+++
@@ -1,5 +0,0 @@
-// SENZA VARIABILI //
-document.getElementById('javascript').innerHTML = 'Output scritto da Javascript'
-
-alert('BENVENUTO/BENVENUTA NELLA PAGINA 2');
-alert('CLICCA DI NUOVO PER CONTINUARE')
